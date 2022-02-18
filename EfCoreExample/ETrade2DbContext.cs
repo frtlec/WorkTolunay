d14645a84d5a2228619a5f76b766647cd67711e0
zafer kırık <zafer.krk@hotmail.com>
2022-02-18 20:37:23 +0300

    Add project files.

---
+++ EfCoreExample/ETrade2DbContext.cs
@@ -0,0 +1,24 @@
+﻿using EfCoreExample.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreExample
+{
+    public class ETrade2DbContext:DbContext
+    {
+    
+        public DbSet<PersonelBilgileri> PersonelBilgileri { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=DESKTOP-934O58E\SQLEXPRESS;Initial Catalog=ETrade2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
---
+++ EfCoreExample/Entity/PersonelBilgileri.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreExample.Entity
+{
+    public class PersonelBilgileri
+    {
+        [Key]
+        public int mno { get; set; }     
+        public string adsoyad { get; set; }     
+        public string telefon { get; set; }
+        public string adres { get; set; }
+        public string email { get; set; }
+        public DateTime? tarihsaat { get; set; }
+    }
+}
